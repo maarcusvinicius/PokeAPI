61c2be26c584eb02158a22ae14864f56815542c8
maarcusvinicius <marcus.editor77@gmail.com>
2023-02-12 12:04:45 -0300

    restart de project, problem with Bulma

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,23 @@
-<script setup>
-  import Headeer from './components/Headeer.vue'
-</script>
-
 <template>
   <div id="app">
-    <Headeer />
-  </div>
 
+  </div>
 </template>
 
-<style scoped>
-#app {
-  font-family: Roboto, sans-serif;
-  text-align: center;
-  color: #09090A;
-  margin-top: 60px;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+<script>
+
+
+export default {
+  components: {
+    //
+  }
 }
+</script>
+
+<style lang="scss">
+
+  body {
+    margin: 0;
+    padding: 0;
+  }
 </style>
