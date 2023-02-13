d2e0c5e7e89748647cc1c37c67703af702852675
maarcusvinicius <marcus.editor77@gmail.com>
2023-02-13 09:18:50 -0300

    exclude axios I no use

--- src/services/axios.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const axiios = axios.create({
-  imageUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/',
-  apiUrl: 'https://pokeapi.co/api/v2/pokemon/',
-});
-
-export default axiios;
